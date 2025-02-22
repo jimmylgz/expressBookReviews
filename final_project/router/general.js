4855bdf37e16c71eb608abf35e76d49adc1b11ac
jimmy li <jimloveba@gmail.com>
2025-02-21 22:51:21 -0500

    done with task 4

--- final_project/router/general.js
+++ final_project/router/general.js
@@ -48,7 +48,21 @@ public_users.get('/author/:author',function (req, res) {
 // Get all books based on title
 public_users.get('/title/:title',function (req, res) {
   //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+  const req_title = req.params.title;
+
+  function findBooksByTitle(titleName) {
+    const result = [];
+    for (const isbn in books) {
+        if (books[isbn].title === titleName) {
+            result.push({isbn, ...books[isbn]});
+            }
+        }
+        return result;
+    }
+
+    const booksByReqTitle = findBooksByTitle(req_title);
+
+    res.send(JSON.stringify(booksByReqTitle, null, 4));
 });
 
 //  Get book review
